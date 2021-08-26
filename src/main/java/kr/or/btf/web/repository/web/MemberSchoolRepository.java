5dff7b12bb432f4eab5e9f201d219a3bd71630af
KimJinuk <kjw1237@limefriends.com>
2021-08-26 19:09:08 +0900

    # MemberSchool.java DB 컬럼 추가로 인한 jpa 컬럼 추가 (tchr_pid) # MemberSchoolRepository.java 프로시저(pr_findTid) 추가함 # TestService.java pr_findTid 부분 추가

--- src/main/java/kr/or/btf/web/repository/web/MemberSchoolRepository.java
+++ src/main/java/kr/or/btf/web/repository/web/MemberSchoolRepository.java
@@ -2,6 +2,8 @@ package kr.or.btf.web.repository.web;
 
 import kr.or.btf.web.domain.web.MemberSchool;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,4 +11,7 @@ public interface MemberSchoolRepository extends JpaRepository<MemberSchool, Long
 
     MemberSchool findByMberPid(Long mberPid);
     MemberSchool findByAreaNmAndSchlNmAndGradeAndBanAndNo(String areaNm, String schlNm, Integer grade, String ban, Integer no);
+
+    @Procedure
+    MemberSchool pr_findTid(String schl_nm , String grade , String ban);
 }
