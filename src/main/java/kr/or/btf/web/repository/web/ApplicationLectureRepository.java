20ce71beda3fe6ad5da71707621547fd37c81d52
KimJaeil <jerrykim@limefriends.com>
2021-09-14 11:58:57 +0900

    tbl_application_leture Entity Create

---
+++ src/main/java/kr/or/btf/web/repository/web/ApplicationLectureRepository.java
@@ -0,0 +1,7 @@
+package kr.or.btf.web.repository.web;
+
+import kr.or.btf.web.domain.web.ApplicationLecture;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationLectureRepository extends JpaRepository<ApplicationLecture, Long> {
+}
