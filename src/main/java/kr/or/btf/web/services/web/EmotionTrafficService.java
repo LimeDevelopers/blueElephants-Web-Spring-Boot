711de978879d8ed312298b6bd1b4577311f8e381
KimJaeil <jerrykim@limefriends.com>
2021-10-06 18:24:59 +0900

    s

--- src/main/java/kr/or/btf/web/services/web/EmotionTrafficService.java
+++ src/main/java/kr/or/btf/web/services/web/EmotionTrafficService.java
@@ -13,13 +13,16 @@ import kr.or.btf.web.domain.web.QEmotionTraffic;
 import kr.or.btf.web.domain.web.QEmotionTrafficLog;
 import kr.or.btf.web.repository.web.EmotionTrafficLogRepository;
 import kr.or.btf.web.repository.web.EmotionTrafficRepository;
+import kr.or.btf.web.utils.DatetimeHelper;
 import kr.or.btf.web.web.form.EmotionTrafficForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -55,17 +58,18 @@ public class EmotionTrafficService extends _BaseService {
         QEmotionTrafficLog qEmotionTrafficLog = QEmotionTrafficLog.emotionTrafficLog;
         QEmotionTraffic qEmotionTraffic = QEmotionTraffic.emotionTraffic;
 
-        StringTemplate formattedDate = Expressions.stringTemplate(
-                "DATE_FORMAT({0}, {1})"
-                , qEmotionTrafficLog.regDtm
-                , ConstantImpl.create("%Y-%m-%d"));
+        LocalDateTime startDatetime = LocalDateTime.of(LocalDate.now().minusDays(1), LocalTime.of(0,0,0)); //어제 00:00:00
+        LocalDateTime endDatetime = LocalDateTime.of(LocalDate.now(), LocalTime.of(23,59,59)); //오늘 23:59:59
 
+        log.info("formattedDate : "+startDatetime);
+        log.info("formattedDate : "+endDatetime);
         OrderSpecifier<Long> orderSpecifier = qEmotionTrafficLog.id.desc();
-        builder.and(Expressions.dateTimeTemplate(LocalDateTime.class,"CONVERT(varchar(10),{0},120)",Expressions.currentTimestamp())
-                .between(
-                        Expressions.dateTimeTemplate(LocalDateTime.class,"CONVERT(varchar(10),{0},120)",qEmotionTrafficLog.regDtm),
-                        Expressions.dateTimeTemplate(LocalDateTime.class,"CONVERT(varchar(10),{0},120)",qEmotionTrafficLog.regDtm)
-                ));
+        builder.and(qEmotionTrafficLog.regDtm.between(startDatetime, endDatetime));
+//        builder.and(Expressions.dateTimeTemplate(LocalDateTime.class,"CONVERT(varchar(10),{0},120)",Expressions.currentTimestamp())
+//                .between(
+//                        Expressions.dateTimeTemplate(LocalDateTime.class,"CONVERT(varchar(10),{0},120)",startDatetime),
+//                        Expressions.dateTimeTemplate(LocalDateTime.class,"CONVERT(varchar(10),{0},120)",endDatetime)
+//                ));
 
         List<EmotionTrafficLog> results = queryFactory
                 .select(Projections.fields(EmotionTrafficLog.class,
@@ -74,50 +78,54 @@ public class EmotionTrafficService extends _BaseService {
                 ))
                 .from(qEmotionTrafficLog)
                 .leftJoin(qEmotionTraffic).on(qEmotionTraffic.id.eq(qEmotionTrafficLog.etfPid))
-
-                .groupBy(formattedDate)
+                .where(builder)
                 .orderBy(orderSpecifier)
                 .fetch();
+        log.info("formattedDate : "+results.size());
         // 데이터 없을경우 <보통>
         if(results.isEmpty()){
             form.setEtfNm("ordinary");
-        }
-        int cnt1 = 0,cnt2 = 0,cnt3 = 0,cnt4 = 0,cnt5 = 0;
-        for(int i=0; i >= results.size(); i++) {
-            if(results.get(i).getEtfPid() == 1) {
-                cnt1++;
+        } else {
+            int cnt1 = 0,cnt2 = 0,cnt3 = 0,cnt4 = 0,cnt5 = 0;
+            for(int i=0; i < results.size(); i++) {
+                log.info("fdfsdfsd : "+results.get(i).getEtfPid());
+                if(results.get(i).getEtfPid() == 1) {
+                    cnt1++;
+                }
+                else if(results.get(i).getEtfPid() == 2) {
+                    cnt2++;
+                }
+                else if(results.get(i).getEtfPid() == 3) {
+                    cnt3++;
+                }
+                else if(results.get(i).getEtfPid() == 4) {
+                    cnt4++;
+                }
+                else if(results.get(i).getEtfPid() == 5) {
+                    cnt5++;
+                }
             }
-            else if(results.get(i).getEtfPid() == 2) {
-                cnt2++;
-            }
-            else if(results.get(i).getEtfPid() == 3) {
-                cnt3++;
-            }
-            else if(results.get(i).getEtfPid() == 4) {
-                cnt4++;
-            }
-            else if(results.get(i).getEtfPid() == 5) {
-                cnt5++;
-            }
-        }
-        // 값이 같을경우는 그냥 보통.
-        form.setEtfNm("ordinary");
-        if(cnt1 > cnt2 && cnt1 > cnt3 && cnt1 > cnt4 && cnt1> cnt5) {
-            form.setEtfNm("good");
-            form.setCnt(cnt1);
-        }
-        if(cnt2 > cnt1 && cnt2 > cnt3 && cnt2 > cnt4 && cnt2> cnt5) {
+
+            // 값이 같을경우는 그냥 보통.
             form.setEtfNm("ordinary");
-            form.setCnt(cnt2);
+            if(cnt1 > cnt2 && cnt1 > cnt3 && cnt1 > cnt4 && cnt1> cnt5) {
+                form.setEtfNm("good");
+                form.setCnt(cnt1);
+            }
+            if(cnt2 > cnt1 && cnt2 > cnt3 && cnt2 > cnt4 && cnt2> cnt5) {
+                form.setEtfNm("ordinary");
+                form.setCnt(cnt2);
+            }
+            if(cnt3 > cnt1 && cnt3 > cnt2 && cnt3 > cnt4 && cnt3 > cnt5) {
+                form.setEtfNm("sad");
+                form.setCnt(cnt3);
+            }
+            if(cnt4 > cnt1 && cnt4 > cnt2 && cnt4 > cnt3 && cnt4 > cnt5) {
+                form.setEtfNm("angry");
+                form.setCnt(cnt4);
+            }
         }
-        if(cnt3 > cnt1 && cnt3 > cnt2 && cnt3 > cnt4 && cnt3 > cnt5) {
-            form.setEtfNm("sad");
-            form.setCnt(cnt3);
-        }
-        if(cnt4 > cnt1 && cnt4 > cnt2 && cnt4 > cnt3 && cnt4 > cnt5) {
-            form.setEtfNm("angry");
-            form.setCnt(cnt4);
-        }
+
         log.info("test@@@2"+ form.getEtfNm());
         return form;
     }
