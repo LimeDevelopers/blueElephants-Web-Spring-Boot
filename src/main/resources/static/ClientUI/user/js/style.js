69a1948d51fa18d8b451b832df29a383bbbab9b3
Suyeon <suyeon@limefriends.com>
2021-11-29 12:00:56 +0900

    20211129_소개 수정

--- src/main/resources/static/ClientUI/user/js/style.js
+++ src/main/resources/static/ClientUI/user/js/style.js
@@ -108,7 +108,7 @@ $(document).ready(function(){
 	})
 
 	//연혁 이미지
-	$('.years_desc.years label').on('click', function(){
+	$('.years_desc label').on('click', function(){
 		$(this).parents().siblings().children().toggleClass('years')
 	})
 
